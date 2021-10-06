9db9835fd36db8faac4269c51f0745d739e58623
John D <supp-supp30+quiz@protonmail.com>
2021-10-06 19:01:36 +0700

    google analyticd

---
+++ gtag.js
@@ -0,0 +1,9 @@
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MR0S2K2YL7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-MR0S2K2YL7');
+</script>
